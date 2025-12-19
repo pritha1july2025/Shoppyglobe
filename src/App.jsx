1ef6e79f1ff7d51a847a0adbac2f2b2aabf97bb0
pritha1july2025 <selvparitha5598@gmail.com>
2025-12-19 11:34:14 +0530

    Update application routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import React from "react";
 
-
+//Update application routing
 const App = () => {
   return (
     <>
