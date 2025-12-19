d176cf76f1345d27f63387d2517e6808c6b438c3
pritha1july2025 <selvparitha5598@gmail.com>
2025-12-19 11:34:58 +0530

    Update application routing in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { RouterProvider } from "react-router-dom";
 import { store } from "./app/store";
 import router from "./routes/router";
 import "./index.css";
-
+//Update application routing
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
